06c48bbcecb223fbe154d179c98b6dabc70c4092
Laura Fumagalli <la_12_msn@hotmail.com>
2023-08-07 20:45:06 -0300

    fix: change position

--- src/components/profileData/ProfilePosition/index.jsx
+++ src/components/profileData/ProfilePosition/index.jsx
@@ -3,7 +3,7 @@ import { Position } from "./styles";
 export const ProfilePosition = () => {
   return (
     <Position layoutId="profile-position" transition={{ duration: 0.7 }}>
-      Dev Fullstack<span>(Jr)</span>
+      Dev Fullstack<span>(Mid-Level)</span>
     </Position>
   );
 };
