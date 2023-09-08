46f78faa454285d63cc3a7699d36edf5874285ac
Laura Fumagalli <la_12_msn@hotmail.com>
2023-09-08 10:49:32 -0300

    feat: add new better backend project(todo list)

--- src/components/MainPages/MainProjects/projectsData.jsx
+++ src/components/MainPages/MainProjects/projectsData.jsx
@@ -1,50 +1,23 @@
-import recipesAppProjectImage from "../../../assets/images/recipes-app.png";
-import talkManagerProjectImage from "../../../assets/images/talk-manager.png";
-
 export const projectsData = [
   {
-    title: "Recipes App",
-    image: recipesAppProjectImage,
-    technologies: [
-      "Html",
-      "Css",
-      "JavaScript",
-      "ReactJS",
-      "Styled Components",
-      "Cypress",
-      "Jest",
-      "RTL",
-      "EsLint"
-    ],
-    links: [
-      {
-        name: "Página",
-        link: "https://main-group-3--frabjous-cactus-64ff07.netlify.app/",
-      },
-      {
-        name: "gitHub",
-        link: "https://github.com/fumagallilaura/recipes-app",
-      },
-    ],
-  },
-  {
-    title: "Talk Manager",
-    image: talkManagerProjectImage,
+    title: "Todo List",
     technologies: [
       "NodeJS",
       "Express",
+      "TypeScript",
       "JavaScript",
       "Docker",
       "EsLint",
       "Debug",
       "Jest",
-      "Frisby",
-      "CRUD"
+      "Mocha",
+      "Chai",
+      "Sinon"
     ],
     links: [
       {
         name: "gitHub",
-        link: "https://github.com/fumagallilaura/talk-manager",
+        link: "https://github.com/fumagallilaura/todo-list",
       },
     ],
   },
